f19380901f3f14df923a472e2cf56b5f1e9dfbef
MCmoefBD <moe_fd@aol.com>
2014-11-04 15:46:17 -0500

    GAME V0.1 Alpha

--- HTML5Application/public_html/script.js
+++ HTML5Application/public_html/script.js
@@ -1,13 +1,45 @@
+var isOn = false;
+var clicks = 0;
+var amount = 1;
+var price = 5;
 $(document).ready(function(){
-    var isOn = false;
         $('.button').click(function(){
             if(!isOn){
            $('.Menu').css('color','red'); 
-           isOn=!isOn;
             }
             else{
            $('.Menu').css('color','green');
-           isOn=!isOn;
             }
+            isOn=!isOn;
+    });
+    
+    $('.add').click(function(){
+        clicks+=amount;
+        refresh();
+    });
+    
+    $('.BuyButtonA').click(function(){
+        if(checkIf(clicks,price)){
+            clicks-=price;
+            price+=10;
+            amount++;
+            refresh();
+            refreshShop();
+        }
     });
 });
+
+ var checkIf = function(a,b){
+        if(a===b||a>b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+var refresh = function(){
+  $('.counter').text('Clicks: '+clicks);    
+};
+var refreshShop = function(){
+    $('.PriceA').text('Price: '+price);
+};
